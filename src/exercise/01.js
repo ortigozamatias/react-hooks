10cab7e553369f4c5b738033028de8b19a032d75
Ortigoza, Matías Nicolás <ortigozamatias@gmail.com>
2021-01-22 23:12:39 -0300

    react-hooks 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,29 +1,30 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import React, { useState } from 'react'
 
-function Greeting() {
+function Greeting ({ initialName = '' }) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = useState(initialName);
 
-  function handleChange(event) {
+  function handleChange (event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
-function App() {
-  return <Greeting />
+function App () {
+  return <Greeting initialName="Matías" />
 }
 
 export default App
